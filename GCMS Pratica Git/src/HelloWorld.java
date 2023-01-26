9f582a141cd17627d37419cfb522e1e5ff3c5f3c
kelly-u <gleicekellymacedodd@gmail.com>
2023-01-26 16:13:14 -0300

    Adicionando mais prints pela branch hotfix

--- GCMS Pratica Git/src/HelloWorld.java
+++ GCMS Pratica Git/src/HelloWorld.java
@@ -6,6 +6,8 @@ public class HelloWorld {
 		System.out.println("Hello, World!");
 		System.out.println("Oi, oi oi!");	
 		System.out.println("Faaaala, nerdocos!");
+		System.out.println("Oi, oi oi!");	
+		System.out.println("Faaaala, nerdocos!");
 	}
 
 }
